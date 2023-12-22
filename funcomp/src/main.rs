b77ce5ad54902a304121a95efecb1727779f6db6
aspirin <f1193968991@126.com>
2023-12-22 13:32:54 +0800

    fix: argument with trailing comma is available

--- funcomp/src/main.rs
+++ funcomp/src/main.rs
@@ -2,7 +2,7 @@ use pest::Parser;
 use funcomp::parser::{SrcParser, Rule};
 
 fn main() {
-    let mut pairs = SrcParser::parse(Rule::expr, "Sin()").unwrap();
+    let mut pairs = SrcParser::parse(Rule::expr, "Sin(a, )").unwrap();
     let expr = SrcParser.expr(pairs.next().unwrap().into_inner());
     println!("{:?}", expr)
 }
