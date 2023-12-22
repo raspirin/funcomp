48644b455e0cd85ce0dc2921958e22d6e376e43a
aspirin <f1193968991@126.com>
2023-12-22 16:37:07 +0800

    fix: process unit enum correctly

--- funcomp_derive/src/lib.rs
+++ funcomp_derive/src/lib.rs
@@ -17,30 +17,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut var_ident_lower = vec![];
     let mut var_fields = vec![];
     let mut var_fields_ident = vec![];
+    let mut unit_ident = vec![];
+    let mut unit_ident_lower = vec![];
 
     if let Data::Enum(data_enum) = item_data {
         for var in data_enum.variants {
-            var_ident_lower.push(Ident::new(
-                &var.ident.to_string().to_lowercase(),
-                var.span(),
-            ));
-            var_ident.push(var.ident);
             let mut var_field_idents = vec![];
             let mut var_tys = vec![];
-            if let Fields::Unnamed(fields) = var.fields {
-                for (counter, field) in fields.unnamed.into_iter().enumerate() {
-                    let span = field.ty.span();
-                    var_tys.push(field.ty);
-                    var_field_idents.push(Ident::new(&format!("a{counter}"), span));
+            let span = var.span();
+            match var.fields {
+                Fields::Unit => {
+                    unit_ident_lower.push(Ident::new(
+                        &var.ident.to_string().to_lowercase(),
+                        span,
+                    ));
+                    unit_ident.push(var.ident);
+                },
+                Fields::Unnamed(fields) => {
+                    var_ident_lower.push(Ident::new(
+                        &var.ident.to_string().to_lowercase(),
+                        span,
+                    ));
+                    var_ident.push(var.ident);
+                    for (counter, field) in fields.unnamed.into_iter().enumerate() {
+                        let span = field.ty.span();
+                        var_tys.push(field.ty);
+                        var_field_idents.push(Ident::new(&format!("a{counter}"), span));
+                    }
                 }
-            } else {
-                return Error::new(
-                    var.fields.span(),
-                    "ItemKind can only used with unnamed fields.",
-                )
-                .into_compile_error()
-                .into();
+                Fields::Named(_) => {
+                    return Error::new(
+                        var.fields.span(),
+                        "ItemKind can only used with unnamed fields.",
+                    )
+                        .into_compile_error()
+                        .into();
+                }
             }
+
             var_fields_ident.push(var_field_idents);
             var_fields.push(var_tys);
         }
@@ -54,6 +68,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl<#item_generic> #item_ident<#item_generic> {
             #(pub fn #var_ident_lower( #(#var_fields_ident: #var_fields, )* ) -> Self {
                 Self::#var_ident( #(#var_fields_ident, )* )
+            })*
+
+            #(pub fn #unit_ident_lower() -> Self {
+                Self::#unit_ident
             })*
         }
     };
